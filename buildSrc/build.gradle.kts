eb401efaaa46cbcdb684636ace1e00b288b3cf22
Zac Sweers <pandanomic@gmail.com>
2020-10-14 00:37:08 -0400

    Demo 6.8-milestone-1 DSL issues

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
 }
 plugins {
     `kotlin-dsl`
+    id("com.squareup.wire")
 }
 
 repositories {
@@ -26,8 +27,23 @@ gradlePlugin {
     }
 }
 
+wire {
+    kotlin {
+        // If you uncomment this, it will fail to compile
+        // If you keep it commented, observe that java sources are generated at
+        // build/generated/source/wire anyway even though we've opted in to kotlin support, suggesting
+        // calling this function in the DSL doesn't actually call Wire's extension
+//        android = false
+    }
+    sourcePath {
+        srcDir("src/main/proto")
+        include("simple.proto")
+    }
+}
+
 dependencies {
-    implementation("com.android.tools.build:gradle:4.0.2")
+    implementation("com.android.tools.build:gradle:4.1.0")
+    implementation("com.squareup.wire:wire-runtime:3.4.0")
 }
 
 println("buildSrc build script classpath kotlin compiler version " + KotlinCompilerVersion.VERSION)
